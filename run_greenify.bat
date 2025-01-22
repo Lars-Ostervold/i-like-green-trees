d355be60f86f02129ceb986adcb037fe8de03d5f
Lars-Ostervold <ostervold.berent@gmail.com>
2025-01-22 09:08:19 -0600

    add .bat

---
+++ run_greenify.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Activate the virtual environment
+call C:\path\to\your\venv\Scripts\activate.bat
+
+REM Run the Python script
+python C:\Users\Oster\Projects\i-like-green-trees\greenify.py
